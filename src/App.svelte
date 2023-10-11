011fd9fa7f0e7198b57dbbee04d6b0a3762c222f
Mitey <zackerycromwell@gmail.com>
2023-10-11 01:29:33 -0400

    added a cat

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
   import svelteLogo from './assets/svelte.svg'
   import viteLogo from '/vite.svg'
   import Counter from './lib/Counter.svelte'
+  let catImg = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fclipartcraft.com%2Fimages%2Fcat-transparent-cute-1.png&f=1&nofb=1&ipt=7fa9b03b8cf7377621f56e28c5d6bf82f00de64041b0c182a3f436cba82e4387&ipo=images"
 </script>
 
 <main>
@@ -11,6 +12,9 @@
     </a>
     <a href="https://svelte.dev" target="_blank" rel="noreferrer">
       <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+    </a>
+    <a href="./src/login" target="_blank" rel="noreferrer"> <!-- how do route? -->
+      <img src={catImg} class="logo cat" alt="Svelte Logo" />
     </a>
   </div>
   <h1>Vite + Svelte</h1>
@@ -40,6 +44,9 @@
   }
   .logo.svelte:hover {
     filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .logo.cat:hover {
+    filter: drop-shadow(0 0 2em #b1afa7)
   }
   .read-the-docs {
     color: #888;
---
+++ src/login.svelte
@@ -0,0 +1,23 @@
+<script>
+
+</script>
+
+
+<body>
+    <div>
+        <h1>Login</h1>
+        <div>
+            <label for="uname">Username
+                <input type="text" name="uname" required>
+            </label>
+
+            <label for="pass">Password
+                <input type="text" name="pass" required>
+            </label>
+
+            <input type="checkbox" name="remember" value="Remember me">
+            
+            <button type="submit">Login</button>
+        </div>
+    </div>
+</body>
