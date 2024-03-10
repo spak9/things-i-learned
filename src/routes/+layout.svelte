3c4b0e74d856e0620e1b311ba86c7eb0ceeca63a
Mitey <zackerycromwell@gmail.com>
2024-03-10 00:29:36 -0500

    Added the start to a navbar and login page w/tailwind

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+    import "../app.css"
+</script>
+
+<nav class="bg-white border-gray-200 top-0 start-0 w-full fixed">
+    <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1 p-3" id="navbar-sticky">
+    <ul class="flex list-none">
+        <li class="mr-6">
+            <a class="text-blue-500 hover:text-blue-800" href="/">Things I Learned</a>
+        </li>
+        <li class="mr-6">
+            <a class="text-blue-500 hover:text-blue-800" href="/">Home</a>
+        </li>
+        <li class="mr-6">
+            <a class="text-blue-500 hover:text-blue-800" href="/login">Login</a>
+        </li>
+    </ul>
+    </div>
+</nav>
+
+<slot></slot>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,37 @@
+<script>
+  import svelteLogo from '../assets/svelte.svg'
+  let catImg = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fclipartcraft.com%2Fimages%2Fcat-transparent-cute-1.png&f=1&nofb=1&ipt=7fa9b03b8cf7377621f56e28c5d6bf82f00de64041b0c182a3f436cba82e4387&ipo=images"
+</script>
+
+<div>
+<a href="https://svelte.dev" target="_blank" rel="noreferrer">
+    <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+</a>
+<a href="/login" target="_self" rel="noreferrer">
+    <img src={catImg} class="logo cat" alt="Svelte Logo" />
+</a>
+</div>
+
+<style lang="postcss">
+    :global(html) {
+        background-color: theme(colors.gray.100);
+    }
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .logo.cat:hover {
+    filter: drop-shadow(0 0 2em #b1afa7)
+  }
+  .read-the-docs {
+    color: #888;
+  }
+</style>
